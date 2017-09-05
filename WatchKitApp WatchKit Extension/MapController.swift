52ac2d648f0f73e25a565a74e49d1d49d5db59f0
Joao <joaovitornaru@hotmail.com>
2017-09-05 14:58:33 -0300

    Map terminado

--- WatchKitApp WatchKit Extension/MapController.swift
+++ WatchKitApp WatchKit Extension/MapController.swift
@@ -8,14 +8,50 @@
 
 import WatchKit
 import Foundation
+import CoreLocation
 
-class MapController: WKInterfaceController {
+class MapController: WKInterfaceController, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet var myMap: WKInterfaceMap!
+    
+    var locationManager: CLLocationManager = CLLocationManager()
     
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestLocation()
+        
         // Configure interface objects here.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        let currentLocation = locations[0]
+        let lat = currentLocation.coordinate.latitude
+        let long = currentLocation.coordinate.longitude
+        
+        
+        let mapLocation = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        
+        let region = MKCoordinateRegionMake(mapLocation, coordinateSpan)
+        
+        myMap.setRegion(region)
+        myMap.addAnnotation(mapLocation, with: WKInterfaceMapPinColor.purple)
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
     
     override func willActivate() {
