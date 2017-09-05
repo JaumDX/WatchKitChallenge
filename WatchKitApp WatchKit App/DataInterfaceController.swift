a92a754e520734ea7688be1cee46913d95af8955
krevi <krevi2796@gmail.com>
2017-09-05 11:09:14 -0300

    Creating the storyboards and swift classes

---
+++ WatchKitApp WatchKit App/DataInterfaceController.swift
@@ -0,0 +1,19 @@
+//
+//  DataInterfaceController.swift
+//  WatchKitApp
+//
+//  Created by Victor Oliveira Kreniski on 05/09/17.
+//  Copyright © 2017 Jaum. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class DataInterfaceConroller: WKInterfaceController {
+    
+    @IBOutlet var slider: WKInterfaceSlider!
+    
+    @IBAction func goToNextDataInterface() {
+        
+    }
+}
---
+++ WatchKitApp WatchKit App/NextDataInterfaceController.swift
@@ -0,0 +1,14 @@
+//
+//  NextDataInterfaceController.swift
+//  WatchKitApp
+//
+//  Created by Victor Oliveira Kreniski on 05/09/17.
+//  Copyright © 2017 Jaum. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class NextDateInterfaceController: WKInterfaceController {
+    
+}
